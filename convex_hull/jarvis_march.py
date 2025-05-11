f9f6a445fddd5b3d1c2eb1c6fa269d5a73df343b
Jaykumaran R <60032500+Jaykumaran@users.noreply.github.com>
2025-05-11 17:09:56 +0530

    Update jarvis_march.py

--- convex_hull/jarvis_march.py
+++ convex_hull/jarvis_march.py
@@ -1,6 +1,10 @@
 import math
 
 def orientation(p1, p2, p3):
+    # slope logic (m1 - m2) is +ve or -ve or 0
+    # +ve - counterclockwise
+    # -ve - clockwiser
+    # 0 - collinear 
     x1, y1, x2, y2, x3, y3 = *p1, *p2, *p3
     d = (y3-y2)*(x2-x1) - (y2-y1)*(x3-x2)
     if d >0:
